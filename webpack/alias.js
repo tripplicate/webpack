a395c35fd969763ec9d05d536b438de3c6bd1414
Danil Sergeev <triplicatesouls@gmail.com>
2021-02-20 01:00:33 +0400

    [remove] Removed files

--- webpack/alias.js
+++
@@ -1,7 +0,0 @@
-let path = require("path")
-
-module.exports = {
-    src: path.resolve(__dirname, "../src"),
-    components: path.resolve(__dirname, "../src/components"),
-    images: path.resolve(__dirname, "../src/assets/images"),
-}
--- webpack/constants.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    sourceFolder: "src",
-    entryFilename: "index.js",
-    outputFolder: "public",
-    outputFilename: "[contenthash].js",
-}
